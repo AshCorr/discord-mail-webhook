54f4b58d25d4ee71904f44039299ce303e22ee53
Ashleigh Carr <ashcorr20@gmail.com>
2021-07-24 12:42:06 +0100

    Initial commit.@

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:14.17.3
+COPY ["package.json", "package-lock.json*", "./index.js"]
+
+RUN npm install --production
+
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,63 @@
+
+require('dotenv').config();
+
+const notifier = require('mail-notifier');
+const axios = require('axios');
+const logger = require('tracer').colorConsole({ format: '[{{timestamp}} {{title}}] (in {{file}}:{{line}}) {{message}}' });
+
+const usernames = process.env.IMAP_USERNAMES.split(",");
+const passwords = process.env.IMAP_PASSWORDS.split(",");
+const githubURL = process.env.DISCORD_WH_URL;
+
+const triggerWebhook = (mail) => {
+
+    const description = mail.text === undefined ? "" : mail.text.substring(0, 1000);
+
+    require("axios").post(githubURL, {
+      content: "",
+      embeds: [
+        {
+          title: mail.headers.subject,
+          author: {
+            name: mail.headers.from
+          },
+          description: description
+        }
+      ]
+    }).then((response) => {
+    }).catch((err) => {
+      logger.error("Error triggering Webhook:" + err);
+    });
+}
+
+logger.info("IMAP Host: " + process.env.IMAP_HOST);
+logger.info("IMAP Port: " + process.env.IMAP_PORT || 141);
+logger.info("IMAP Check TLS: " + (process.env.IMAP_TLS || "true"));
+logger.info("IMAP Check TLS: " + (process.env.IMAP_MARK_SEEN || "false"));
+
+usernames.forEach((username, index) => {
+    const imap = {
+        user: username,
+        password: passwords[index],
+        host: process.env.IMAP_HOST,
+        port: process.env.IMAP_PORT || 141,
+        tls: process.env.IMAP_TLS === "true" ? true : false,
+        autotls: 'always',
+        markSeen: process.env.IMAP_MARK_SEEN === "false" ? false : true
+    };
+
+    notifier(imap)
+        .on('connected',() => {
+          logger.info("Connected " + username + ". Listening for emails.");
+        })
+        .on('error', (err) => {
+          throw error;
+        })
+        .on('mail', triggerWebhook)
+    .start();
+})
+
+
+
+
+
